3a02972d1373e2b4e6f80a15b765c1a7b915603c
Mauricio Ferrari <m10ferrari1200@gmail.com>
2024-08-10 23:29:43 -0300

    add generate translation

---
+++ lang/qcine.pro
@@ -0,0 +1,5 @@
+SOURCES = ../src/QCine.cpp  \
+          ../src/settings/EnvironmentVariable.cpp \
+          ../src/settings/Settings.cpp
+
+TRANSLATIONS = qcine_pt.ts
---
+++ lang/qcine_pt.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>QCine::QCine</name>
+    <message>
+        <location filename="../src/QCine.cpp" line="446"/>
+        <source>Open Files</source>
+        <translation>Abrir Arquivos</translation>
+    </message>
+    <message>
+        <location filename="../src/QCine.cpp" line="452"/>
+        <source>Settings</source>
+        <translation>Configurações</translation>
+    </message>
+</context>
+<context>
+    <name>QCineEnvironmentVariable::EnvironmentVariable</name>
+    <message>
+        <location filename="../src/settings/EnvironmentVariable.cpp" line="82"/>
+        <source>QMediaPlayer Backend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/EnvironmentVariable.cpp" line="89"/>
+        <source>Variables</source>
+        <translation>Variáveis</translation>
+    </message>
+</context>
+<context>
+    <name>QCineSettings::Settings</name>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="57"/>
+        <source>Include Default Background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="63"/>
+        <source>Double Gradient Strength</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="69"/>
+        <source>Disable Effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="73"/>
+        <source>Disable Background Effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="78"/>
+        <source>Effect Duration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="84"/>
+        <source>Transition Duration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="90"/>
+        <source>Hide Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="96"/>
+        <source>Opacity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="102"/>
+        <source>Blur</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="108"/>
+        <source>Border Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="143"/>
+        <source>Multimedia Engine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="145"/>
+        <source>Current Engine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="149"/>
+        <source>Themes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="151"/>
+        <source>Program Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="159"/>
+        <source>Background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="161"/>
+        <source>Background Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="167"/>
+        <source>Player Controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="172"/>
+        <source>Transition Effects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="178"/>
+        <source>Background Effects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="207"/>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="208"/>
+        <source>Effects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="209"/>
+        <source>Performance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/settings/Settings.cpp" line="210"/>
+        <source>Environment Variables</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
